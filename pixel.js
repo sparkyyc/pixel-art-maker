326586a81509c3101a4501199b44de5eebaf6dd0
Christa Sparks <christa@Christas-MacBook-Pro.local>
2018-08-30 11:35:16 -0600

    Change to using inline styles via javascript, reordered palette

--- pixel.js
+++ pixel.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
   let container = document.createElement('div')
   container.classList.add('maker')
   container.classList.add('container')
+  container.classList.add('mt-4')
   document.body.appendChild(container)
-  const rowNum = 12
+  const rowNum = 26
   for (let j = 0; j < rowNum; j++) {
     container.appendChild(createRow(j))
   }
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   function createColumns(row, j) {
     // loop to create columns
-    const colNum = 12
+    const colNum = 50
     for (let i = 0; i < colNum; i++) {
       // make the bootstrap column
       let col = document.createElement('div')
@@ -32,31 +33,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
   function createSquare() {
     let div = document.createElement('div')
     div.classList.add('square')
+    div.setAttribute('style', 'background-color: white');
     return div
   }
 
-// Look into creating inline styles
 
-  let colorOptions = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'black', 'brown', 'gray'];
-  let palette = document.createElement('div');
-  palette.classList.add('palette');
-  palette.classList.add('center');
-  document.body.appendChild(palette);
-  for (let x = 0; x < colorOptions.length; x++) {
-    let colors = document.createElement('span');
-    colors.setAttribute('value', colorOptions[x]);
-    colors.classList.add(colorOptions[x]);
-    colors.classList.add('circle');
-    palette.appendChild(colors);
-  }
 
-  let colorShower = document.createElement('p');
-  colorShower.innerText = 'Color Picked: ';
-  palette.appendChild(colorShower);
+let colorOptions = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
 
-  let currentColor = document.createElement('span');
-  currentColor.classList.add('rectangle');
-  palette.appendChild(currentColor);
+let palette = document.createElement('div');
+palette.classList.add('palette');
+palette.classList.add('center');
+palette.classList.add('container');
+document.body.appendChild(palette);
+
+let picker = document.createElement('div');
+picker.classList.add('row');
+palette.appendChild(picker);
+
+let colorShower = document.createElement('p');
+colorShower.innerText = 'Color Picked: ';
+colorShower.classList.add('col');
+colorShower.classList.add('bold');
+colorShower.classList.add('align-self-center');
+//colorShower.classList.add('c');
+picker.appendChild(colorShower);
+
+let currentColor = document.createElement('span');
+currentColor.classList.add('rectangle');
+currentColor.classList.add('col');
+picker.appendChild(currentColor);
+
+let options = document.createElement('div');
+options.classList.add('row');
+palette.appendChild(options);
+
+for (let x = 0; x < colorOptions.length; x++) {
+  let colors = document.createElement('span');
+  colors.setAttribute('style', 'background-color: ' + colorOptions[x]);
+  colors.setAttribute('value', colorOptions[x]);
+  colors.classList.add('circle');
+  palette.appendChild(colors);
+}
+
 
 
   let squareClicked = document.querySelector('.maker')
@@ -65,15 +84,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
   let color = [];
   colorPicked.addEventListener('click', (e) => {
     color.unshift(e.target.getAttribute('value'));
-    currentColor.className = '';
-    currentColor.classList.add('rectangle');
-    currentColor.classList.add(color[0]);
+    currentColor.removeAttribute("style")
+    currentColor.setAttribute('style', 'background-color: ' + color[0]);
   })
 
 
 
+
   squareClicked.addEventListener('click', (evt) => {
-    evt.target.classList.add(color[0]);
+    evt.target.setAttribute('style', 'background-color: ' + color[0]);
   });
 });
 
